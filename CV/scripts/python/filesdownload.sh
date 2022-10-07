d18840fd66b1910b142ed0513501c4f4f33c67a0
alexmedinait <alex.medina@iteso.mx>
2022-10-07 18:29:13 -0500

    Update filesdownload.sh

--- CV/scripts/python/filesdownload.sh
+++ CV/scripts/python/filesdownload.sh
@@ -3,5 +3,4 @@ source /Users/alex/Documents/DS/ds-env/bin/activate
 export KAGGLE_USERNAME=alexmedinaxn
 export KAGGLE_KEY=0ec4569d8183582c333738f979e2dbf2
 
-#kaggle datasets download vishalsubbiah/pokemon-images-and-types -w -p /Users/alex/Documents/GitHub/IDI/CV/data_raw --unzip
 kaggle datasets download agrigorev/clothing-dataset-full -w -p /Users/alex/Documents/GitHub/IDI/CV/data_raw --unzip
